4e08d616d55a0e1dba1e60d089638fcb77c0f6ea
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2019-08-06 22:16:48 -0400

    agregada discretizacion y comparacion con metodo tree

--- test.R
+++ test.R
@@ -4,16 +4,37 @@ library(rpart.plot)
 library(tidyverse)
 library(caret)
 library(e1071)
+library(tree)
 
 set.seed(8512)
 
 Data <- read.csv(file="./DatosInforme19.csv", header=TRUE, sep=";")
-Data_Sample <- sample_frac(Data, .7)
+
+#Data$VTR <- as.factor(Data$VTR)
+Data$VTR <- factor(Data$VTR, levels=c(0:2), ordered=TRUE)
+#Data$TiempoLibre <- as.factor(Data$TiempoLibre)
+Data$TiempoLibre <- factor(Data$TiempoLibre, levels=c("Nada", "Poco", "Normal", "Mucho", "Demasiado"), ordered=TRUE)
+#Data$Salud <- as.factor(Data$Salud)
+Data$Salud <- factor(Data$Salud, levels=c("Muy Mala", "Suficiente", "Normal", "Buena", "Muy Buena"), ordered=TRUE)
+#Data$Carrete <- as.factor(Data$Carrete)
+Data$Carrete <- factor(Data$Carrete, levels=c("Nada", "Poco", "Normal", "Mucho", "Demasiado"), ordered=TRUE)
+#Data$Inasistencias <- as.integer(Data$Inasistencias)
+Data$Inasistencias <- factor(Data$Inasistencias, levels=c(0:10), ordered=TRUE)
+#Data$HorasEstudioSemanal <- as.factor(Data$HorasEstudioSemanal)
+Data$HorasEstudioSemanal <- factor(Data$HorasEstudioSemanal, levels=c("<2 hr", "2-5 hr", "5-10 hr", ">10 hr"), ordered=TRUE)
+Data$NotaFinal <- as.factor(Data$NotaFinal)
+Data$Sexo <- as.factor(Data$Sexo)
+
+
+
+Data_Sample <- sample_frac(Data, 1)
 Data_Test <- setdiff(Data, Data_Sample)
 
 # Entera
 arbol <- rpart(NotaFinal ~ Sexo + HorasEstudioSemanal + VTR + TiempoLibre + Carrete
  + Salud + Inasistencias, data = Data_Sample, method = "class")
+#arbol <- tree(NotaFinal ~ Sexo + HorasEstudioSemanal + VTR + TiempoLibre + Carrete
+#               + Salud + Inasistencias, data = Data_Sample)
 
 # Sin VTR
 #arbol <- rpart(NotaFinal ~ Sexo + HorasEstudioSemanal + TiempoLibre + Carrete
@@ -24,9 +45,9 @@ arbol <- rpart(NotaFinal ~ Sexo + HorasEstudioSemanal + VTR + TiempoLibre + Carr
 # + Salud, data = Data_Sample, method = "class")
 
 arbol
+#plot(arbol)
+#text(arbol, pretty = 2)
 rpart.plot(arbol, type = 4, extra = 103)
-#plot(dtm)
-#text(dtm)
 
 prediccion_1 <- predict(arbol, newdata = Data_Test, type = "class")
 confusionMatrix(prediccion_1, Data_Test[["NotaFinal"]])
